bb42b52877a09e4de8a7b69d3e889d7c153cc734
roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2024-05-31 22:27:13 +0200

    Poprawki, działa

--- W13/tf_lite_onprem.py
+++ W13/tf_lite_onprem.py
@@ -4,11 +4,12 @@ import tflite_runtime.interpreter as tflite
 x_test = []
 # Wczytaj dane MNIST
 for i in range(5):
-    x_test.append(np.random.rand(28,28))
+    x_test.append(np.random.rand(1,784))
 x_test = np.array(x_test)
+x_test = x_test.astype(np.float32)
 
 # Wczytaj model TFLite
-interpreter = tflite.Interpreter(model_path="model.tflite")
+interpreter = tflite.Interpreter(model_path="converted_model.tflite")
 interpreter.allocate_tensors()
 
 # Pobierz dane wejściowe i wyjściowe
@@ -17,7 +18,7 @@ output_details = interpreter.get_output_details()
 
 # Przeprowadź inferencję na wybranych obrazkach
 for i, img in enumerate(x_test):
-    img = np.expand_dims(img, axis=0)  # Dodaj wymiar batch
+    # img = np.expand_dims(img, axis=0)  # Dodaj wymiar batch
     interpreter.set_tensor(input_details[0]['index'], img)
     interpreter.invoke()
     
